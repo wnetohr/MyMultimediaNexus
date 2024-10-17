547bc9575fd99bab347f023cc58d88556fde5455
wnetohr <washingtonbdarocha@gmail.com>
2024-10-17 13:20:24 -0300

    start spring project

---
+++ src/main/java/com/washington/neto/MyMultimediaNexus/MyMultimediaNexusApplication.java
@@ -0,0 +1,13 @@
+package com.washington.neto.MyMultimediaNexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMultimediaNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMultimediaNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/washington/neto/MyMultimediaNexus/MyMultimediaNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washington.neto.MyMultimediaNexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMultimediaNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
